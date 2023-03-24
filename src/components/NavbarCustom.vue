b917d18fe9299f47de6f214597e89b873c8b34f5
FrancescoCauzzi <elblanco.fc@gmail.com>
2023-03-24 16:46:58 +0100

    feat: navbar done

--- src/components/NavbarCustom.vue
+++ src/components/NavbarCustom.vue
@@ -20,13 +20,17 @@ export default {
 </script>
 
 <template>
-  <div class="__nav-bar-cnt d-flex p-3 align-items-center">
-    <div class="__logo-wrapper w-25">
+  <div class="__nav-bar-cnt d-flex align-items-center">
+    <div class="__logo-wrapper w-25 p-3">
       <img src="/img/dc-logo.png" alt="cannot-render-image" srcset="" />
     </div>
     <div class="__links-wrapper w-75">
-      <ul class="d-flex gap-4 flex-wrap">
-        <li class="fw-bold" v-for="link in linksList" :key="link.id">
+      <ul class="d-flex gap-4">
+        <li
+          class="fw-bold d-flex align-items-center"
+          v-for="link in linksList"
+          :key="link.id"
+        >
           <a href="">
             {{ link.toUpperCase() }}
           </a>
@@ -37,9 +41,29 @@ export default {
 </template>
 
 <style scoped>
+.__nav-bar-cnt {
+  height: 135px;
+}
 a {
   cursor: pointer;
   text-decoration: none;
   color: black;
+}
+
+.__links-wrapper {
+  height: 100%;
+}
+
+ul {
+  height: 100%;
+}
+li {
+  height: 100%;
+}
+
+li:hover {
+  border-bottom: 5px solid #0282f9;
+  padding-top: 5px;
+  color: #0282f9;
 }
 </style>
